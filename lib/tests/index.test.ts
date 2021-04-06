070c2c22ed9bf20ba897c165c183d1d1d859b79b
Albert Brand <abrand@xebia.com>
2021-04-06 10:42:17 +0200

    Add throws test

--- lib/tests/index.test.ts
+++ lib/tests/index.test.ts
@@ -2,14 +2,14 @@ import { defineComponent, h, ref } from "vue";
 import { defineContext, provideContext, useContext } from "../src";
 import { mount } from "@vue/test-utils";
 
-test("usage of defineContext, provideContext, useContext", () => {
-  const testContext = defineContext(() => {
-    const test = ref("hello");
-    return {
-      test,
-    };
-  });
+const testContext = defineContext(() => {
+  const test = ref("hello");
+  return {
+    test,
+  };
+});
 
+test("usage of defineContext, provideContext, useContext", () => {
   const componentUsingContext = defineComponent({
     setup() {
       const context = useContext(testContext);
@@ -28,4 +28,21 @@ test("usage of defineContext, provideContext, useContext", () => {
 
   const testDiv = wrapper.get("div");
   expect(testDiv.text()).toBe("hello");
+});
+
+test("throws when context is not provided", () => {
+  const componentUsingContext = defineComponent({
+    setup() {
+      expect(() => useContext(testContext)).toThrow();
+      return () => h("div");
+    },
+  });
+
+  mount(componentUsingContext, {
+    global: {
+      config: {
+        warnHandler: () => undefined,
+      },
+    },
+  });
 });
